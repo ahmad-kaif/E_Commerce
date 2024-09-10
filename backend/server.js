4eaf72a8cc9d2d30dc9e145267020d88fa141638
Ahmad Kaif <heyahmad611@gmail.com>
2024-09-10 17:47:50 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+
 const app =  express();
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
-
-
-
-
+    console.log("")
 
 
 });
